30079423c50d6b5aa8d20931a9cf4ae1542af669
Mike Rapp <mikejrapp@gmail.com>
2016-08-27 19:00:05 -0600

    got function working

--- nonRecursive_Michael_Rapp/nonRecursive_Michael_Rapp/Source.cpp
+++ nonRecursive_Michael_Rapp/nonRecursive_Michael_Rapp/Source.cpp
@@ -17,25 +17,28 @@ int main() {
 
 int fibonacci(int num) {
 	int loopCounter = num;
-	int fibNum;
+	int base = 0;
+	int adder = 1;
+	int answer;
 
-	if (num == 0) {
-		return 0;
-	}
-	else if (num == 1) {
-		return 1;
-	}
-	else if (num >= 2){
-		
-		while (loopCounter > 0) {
-			fibNum = (num - 1) + (num - 2);
-			loopCounter--;
+	while (loopCounter > 0) {
+		if (base <= 2) {
+			answer = base + adder;
+			//since the adder will remain one until the answer becoms 3 there is no need to update the adder.
+			if(answer < 3)
+				//this prevents the base, adder and answer from all becoming three.
+				base = answer;
 		}
+		if (answer >= 3) {
+			//once the answer becomes three the logic changes a bit. The adder will now hold the number that preceeds the base before the
+			//arithmetic begins.
+			adder = base;
+			base = answer;
+			answer = base + adder;
+		}
+		loopCounter--;
+	}
 
-		return fibNum;
-	}
-	else {
-		return 0;
-	}
+	return answer;
 }
 
