fb2380426b05e1ae1f8c6b2b5a0a8f4d8e75e970
Mike Rapp <mikejrapp@gmail.com>
2016-08-28 09:01:52 -0600

    working solution

--- nonRecursive_Michael_Rapp/nonRecursive_Michael_Rapp/Source.cpp
+++ nonRecursive_Michael_Rapp/nonRecursive_Michael_Rapp/Source.cpp
@@ -38,22 +38,26 @@ int fibonacci(int num) {
 			if (base <= 2) {
 				answer = base + adder;
 				//since the adder will remain one until the answer becoms 3 there is no need to update the adder.
-				if (answer < 3)
+				if (answer < 3) {
 					//this prevents the base, adder and answer from all becoming three.
 					base = answer;
-			}
+				}//end if answer < 3
+					
+			}//end if base <= 2
+
 			if (answer >= 3) {
-				//once the answer becomes three the logic changes a bit. The adder will now hold the number that preceeds the base before the
+				//once the answer becomes three the logic changes a bit. The adder will now hold the number that precedes the base before the
 				//arithmetic begins.
 				adder = base;
 				base = answer;
 				answer = base + adder;
-			}
-			loopCounter--;
-		}
-	}
-	
+			}//end if answer >= 3
 
-	return answer;
+			loopCounter--;
+		}//end while 
+	}//end if num > 2
+	
+	//return answer;//this is the working response, but isn't returning the right number in the sequence with 4.
+	return base; //since the base is the actual working position in the sequence this should be returned.
 }
 
